1cf3a95d6d797e803ad32dfc7657fa16dfdad5b7
Rodrigo <rodrigo.zanetti@hotmail.com>
2024-09-18 10:12:33 -0300

    Trying to run the server on Render

--- FlightlyAPI.py
+++ FlightlyAPI.py
@@ -1,7 +1,9 @@
 #imports
 from flask import Flask, jsonify , request
 from selenium import webdriver  
+import chromedriver_autoinstaller as chromedriver
 import time
+from datetime import datetime
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
@@ -9,15 +11,22 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException, NoSuchElementException
+from selenium.webdriver.common.action_chains import ActionChains
 
-
-
-#setup
-PATH = "C:/Program Files (x86)/chromedriver.exe" 
+#PATH = "C:/Program Files (x86)/chromedriver.exe" 
+chromedriver.install()
 options = Options()
 options.add_experimental_option("detach", True) 
-driver = webdriver.Chrome(service=Service(PATH), options=options)
+options.add_argument('--lang=pt-BR')
+options.add_argument('--proxy-server="direct://"')
+options.add_argument('--proxy-bypass-list=*')
+
+options.add_argument('--log-level=3')  # Suppress log level to show only severe errors
+options.add_experimental_option('excludeSwitches', ['enable-logging'])
+
+driver = webdriver.Chrome(options=options)
 driver.maximize_window()
+actions = ActionChains(driver)
 app = Flask(__name__)
 
 def check_exists_by_xpath(driver, xpath):
@@ -31,25 +40,42 @@ def check_exists_by_xpath(driver, xpath):
 @app.route('/')
 def home():
     return "ijfeuharhu"
-def scrape_flight_data(cards, response):
+def scrape_flight_data(cards, response, filters = None):
         for card in cards:
-            travel_time = card.find_elements('xpath', ".//span[@class='mv1WYe']//span[@role='text']")
-            take_off = travel_time[0].text
-            arrival = travel_time[1].text
+            if(check_exists_by_xpath(driver, ".//div[contains(@class, 'YMlIz FpEdX')]//span")):
+                price = card.find_elements('xpath', ".//div[contains(@class, 'YMlIz FpEdX')]//span")[0]
+            else:
+                price = driver.find_element('xpath', "//div[@class='QORQHb']/span")
+            price = price.text.replace("R$ ","").replace(".","")
+            if(filters and "max_price" in filters):
+                if(int(price) > filters["max_price"]):
+                    break
+            if(filters and "min_price" in filters):
+                if(int(price) < filters["min_price"]):
+                    continue
             travel_company = card.find_elements('xpath', ".//div[@class='sSHqwe tPgKwe ogfYpf']/span[not(@class)]")[0]
             travel_company = travel_company.text
+            #maybe i'll have to check if filters["company"] exists instead of using in
+            if(filters and "company" in filters):
+                if(travel_company not in filters["company"]):
+                    continue
+            travel_time = card.find_elements('xpath', ".//span[@class='mv1WYe']//span[@role='text']")
+            take_off = travel_time[0].text
+            if(filters and "take_off" in filters):
+                date_format = "%H:%M"
+                if(datetime.strptime(take_off, date_format) > datetime.strptime(filters["take_off"],date_format)):
+                    continue
+            stops = card.find_element('xpath', ".//div[@class='EfT7Ae AdWm1c tPgKwe']//span[@class='ogfYpf']")
+            stops = stops.text
+            if(filters and "stops" in filters):
+                if(int(stops) != filters["stops"]):
+                    continue
+            arrival = travel_time[1].text
             airports = card.find_elements('xpath', ".//div[@class='']")
             airport_from = airports[0].get_attribute('innerHTML')
             airport_to = airports[1].get_attribute('innerHTML')
             duration = card.find_element('xpath', ".//div[@class='gvkrdb AdWm1c tPgKwe ogfYpf']")
             duration = duration.text
-            stops = card.find_element('xpath', ".//div[@class='EfT7Ae AdWm1c tPgKwe']//span[@class='ogfYpf']")
-            stops = stops.text
-            if(check_exists_by_xpath(driver, ".//div[contains(@class, 'YMlIz FpEdX')]//span")):
-                price = card.find_elements('xpath', ".//div[contains(@class, 'YMlIz FpEdX')]//span")[0]
-            else:
-                price = driver.find_element('xpath', "//div[@class='QORQHb']/span")
-            price = price.text
             
             response.append({
                 "take_off": take_off,
@@ -60,7 +86,7 @@ def scrape_flight_data(cards, response):
                 "airport_from": airport_from,
                 "airport_to": airport_to,
                 "stops": stops
-            })
+                })
             
         return response
     
@@ -173,7 +199,7 @@ def flights():
     #start scrapping
     expand = driver.find_element('xpath', "//div[contains(@class, 'zISZ5c')]")
     expand.click()
-    time.sleep(2.5)
+    time.sleep(6)
     cards = driver.find_elements('xpath', "//div[@class='yR1fYc']")
     
     response = []
@@ -212,7 +238,7 @@ def phase3():
         reservation_dict.update({"company" : reservation.find_element('xpath', ".//div[@class='ogfYpf AdWm1c']").text})
         others.append(reservation_dict)
     response.append({"others" : others})
-    return response
+    return jsonify(response)
 @app.get("/flights/redirect")
 def redirect_flight():
     driver.get(request.json['url'])
@@ -224,6 +250,16 @@ def redirect_flight():
 @app.get("/flights/filter")
 def apply_filter():
     driver.get(request.json['url'])
+    time.sleep(1)
+    expand = driver.find_element('xpath', "//div[contains(@class, 'zISZ5c')]")
+    expand.click()
+    time.sleep(4)
+    filters = request.json['filters']
+    response = scrape_flight_data(driver.find_elements('xpath', "//div[@class='yR1fYc']"), [], filters)
+    return jsonify(response)
+
+    #price_slider -> //input[@class='VfPpkd-YCNiv']/
+    
 #running
 @app.get("/hotels")
 def hotels():
@@ -255,10 +291,3 @@ def hotels():
     return "hello world"
 if __name__ == "__main__":
     app.run()
-
-
-
-
-
-
-
